c743ef12f61588f986f40853673a9fab4721e403
Sasha <oleksandrsy@gmail.com>
2023-01-19 16:09:17 +0200

    Create MusicTrack class

---
+++ Models/MusicTrack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spotify.Models
+{
+    internal class MusicTrack
+    {
+        public string Name { get; set; }
+        public Artist Creator { get; set; }
+        public string SoundtrackPath { get; set; }
+        public Genre Genre { get; set; }
+        public readonly DateTime CreationDate;
+
+    }
+}
